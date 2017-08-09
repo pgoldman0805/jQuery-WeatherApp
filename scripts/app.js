a215d8f326264f5df40d54b42699ad7ae511b1e3
Paul Goldman <paulgoldmanmusic@gmail.com>
2017-08-09 10:10:43 -0500

    ui and graphic update
    Added code-based weather images, background

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,8 @@
+// use the CODE (two digit number) to replace the '30' in the below example
+// EXAMPLE: var imgBaseURL"<img src='http://l.yimg.com/a/i/us/we/52/30.gif'/>"
+var imgBaseURL = "<img src='http://l.yimg.com/a/i/us/we/52/";
+var imgBackURL = ".gif'/>";
+
 var _baseURL = "https://query.yahooapis.com/v1/public/yql?q=";
 var _geoLocationQuery = "select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D'";
 var latitude = "";
@@ -18,7 +23,7 @@ $(document).ready(function(){
 
         // create URL with user's query strings
         var userInput = $("#userLocation").val();
-        console.log(userInput);
+        // console.log(userInput);
         geoLocationQuery += encodeURI(userInput);
         geoLocationQuery += "')&format=json";
         baseURL += geoLocationQuery;
@@ -76,34 +81,47 @@ function setFields(){
     // get array of forecasts
     var forecasts = jsonData.forecast;
     var para = "";
-    console.log(forecasts.length);
-    for (var i = 0; i < 5; i++){
+    // console.log(forecasts.length);
+    for (var i = 0; i < 10; i++){
         // var para = "<div class='row'>";
         // if (i === 0)
         //     para += "<div class='row'>";
-        para += "<div class='col-md-2'>";
+        para += "<div class='col-md-2 text-center'>";
         para += "<p><strong><span>";
-        para += forecasts[i].day + ', ' + forecasts[i].date;
-        para += ": </span></strong><br/>";
-        para += "High of " + forecasts[i].high + "&#176;F <br/>";
-        para += "Low of " + forecasts[i].low + "&#176;F <br/>";
-        para += forecasts[i].text;
-        para += "</p></div>";
+        para += forecasts[i].day + '<br/>' //+ forecasts[i].date.month;
 
-        console.log(para);
-        $("#forecast").append(para);
-        para = "";
-    }
-    para += "<div class='row text-center'>";
-    for (var i = 5; i < forecasts.length; i++){
-        // var para = "<div class='row'>";
-        para += "<div class='col-md-2'>";
-        para += "<p><strong><span>";
-        para += forecasts[i].day + ', ' + forecasts[i].date;
+        // format the date to just capture month, then day
+        var dateFields = forecasts[i].date.split(' ');
+        var formattedDate = dateFields[1] + ' ' + dateFields[0];
+        para += formattedDate;
+        // console.log(dateFields);
+
         para += ": </span></strong><br/>";
-        para += "High of " + forecasts[i].high + "F <br/>";
-        para += "Low of " + forecasts[i].low + "F <br/>";
-        para += forecasts[i].text;
+        para += "<span class='red'>" + forecasts[i].high + "&#176;F</span><br/>";
+        para += "<span class='blue'>" + forecasts[i].low + "&#176;F</span><br/>";
+        // APPEND THE IMAGE
+        var imgCode = forecasts[i].code;
+        var imgURL = imgBaseURL + imgCode + imgBackURL;
+        // console.log(imgURL);
+        para += imgURL;
+        // para += forecasts[i].text;
+
+    //     para += "</p></div>";
+    //
+    //     console.log(para);
+    //     $("#forecast").append(para);
+    //     para = "";
+    // }
+    // para += "<div class='row text-center'>";
+    // for (var i = 5; i < forecasts.length; i++){
+    //     // var para = "<div class='row'>";
+    //     para += "<div class='col-md-2'>";
+    //     para += "<p><strong><span>";
+    //     para += forecasts[i].day + ', ' + forecasts[i].date;
+    //     para += ": </span></strong><br/>";
+    //     para += "High of <span class='red'>" + forecasts[i].high + "&#176;F</span><br/>";
+    //     para += "Low of <span class='blue'>" + forecasts[i].low + "&#176;F</span><br/>";
+    //     para += forecasts[i].text;
         para += "</p></div></div>";
 
         $("#forecast").append(para);
